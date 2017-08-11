bc81c30de60b60b4338a35fe308dc5baa52c401c
Fredrik Pettersen <fredrik.fumler@gmail.com>
2017-08-11 17:22:05 +0200

    test

--- pages/character.js
+++ pages/character.js
@@ -2,7 +2,20 @@ import React from "react"
 
 import Layout from "../components/Layout"
 
-export default () =>
-	<Layout>
-		<div>world</div>
-	</Layout>
+export default class Character extends React.Component {
+	static async getInitialProps({ req }) {
+		return {
+			req,
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<p>
+					{this.props.req}
+				</p>
+			</div>
+		)
+	}
+}
